6b997bbe8f5bf63f2d02b632be4c0c4d937aef38
Pongsiri Chatkaew <pongsiri.ch@inet.co.th>
2021-08-13 18:51:18 +0700

    finish find the planet

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const parse = require('csv-parse');
 const fs = require('fs');
 
-const results = [];
+const habitablePlanets = [];
+
+// Review of best Habitable Planets
+// https://www.centauri-dreams.org/2015/01/30/a-review-of-the-best-habitable-planet-candidates/
+
+function isHabitablePlanet(planet) {
+  return (
+    planet['koi_disposition'] === 'CONFIRMED' &&
+    planet['koi_insol'] > 0.36 &&
+    planet['koi_insol'] < 1.11 &&
+    planet['koi_prad'] < 1.6
+  );
+}
+
 fs.createReadStream('kepler_data.csv')
   .pipe(
     parse({
@@ -11,13 +24,19 @@ fs.createReadStream('kepler_data.csv')
   )
   .on('data', (data) => {
     // console.log('chunk', data);
-    results.push(data);
+    if (isHabitablePlanet(data)) {
+      habitablePlanets.push(data);
+    }
   })
   .on('error', (err) => {
     console.log('error', err);
   })
   .on('end', () => {
-    console.log('results', results);
-    console.log('done');
+    console.log(
+      habitablePlanets.map((planet) => {
+        return planet['kepler_name'];
+      })
+    );
+    console.log(`${habitablePlanets.length} habitable planets found!`);
   });
 // parse();
